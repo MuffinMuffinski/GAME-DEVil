f1da7ffa5786bf068b27dd7af05b9512194b69e9
Merve <meszaros@threaks.com>
2023-05-06 15:58:47 +0200

    Countdown

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject DialoguePanel;
+    [SerializeField] private TMP_Text timerLabel;
     [SerializeField] private List<InteractableItem> itemList;
+
+    private float targetTime = 20f;
+    private bool isPaused;
     
     public static GameManager Instance { get; private set; }
     private void Awake()
@@ -19,6 +25,29 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (!isPaused)
+        {
+            targetTime -= Time.deltaTime;
+            
+            var timeString = targetTime < 10 ? $"0{(int)targetTime}" : ((int)targetTime).ToString();           
+            timerLabel.text = $"00:{timeString}";
+        }
+ 
+        if (targetTime <= 0.0f)
+        {
+            TimerEnded();
+        }
+    }
+
+    void TimerEnded()
+    {
+        isPaused = true;
+        
+        //TODO end game
+    }
+
     private void OnDestroy()
     {
         if (Instance == this)
@@ -31,7 +60,15 @@ public class GameManager : MonoBehaviour
     public void OnItemClick()
     {
         //Pause timer
+        isPaused = true;
+
         //Play corresponding dialogue > safe item as already clicked?
         //DialoguePanel.gameObject.SetActive(true);
+    }
+    
+    public void ContinueTimer()
+    {
+        //Pause timer
+        isPaused = false;
     }
 }
